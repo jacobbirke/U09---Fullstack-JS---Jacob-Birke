b8f5cee4603aeb3993d9061f58bd8347079743f3
jacobbirke <birkejacob@gmail.com>
2024-09-18 13:50:43 +0200

    format fix

--- api/index.js
+++ api/index.js
@@ -4,13 +4,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT;
 
-const mongoose = require("mongoose"); 
+const mongoose = require("mongoose");
 
 //connect db
-mongoose.connect(process.env.MONGOOSEDB_RUL)
+mongoose
+  .connect(process.env.MONGOOSEDB_RUL)
   .then(() => console.log("db connected"))
   .catch((err) => {
-    console.error("Connection error", err); 
+    console.error("Connection error", err);
   });
 
 app.listen(PORT || 9000, () => {
