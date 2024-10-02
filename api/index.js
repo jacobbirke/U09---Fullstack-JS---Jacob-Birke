d4133a7a13ab24a21af02733d3fd4ccd3e37fa34
jacobbirke <birkejacob@gmail.com>
2024-10-02 11:10:42 +0200

    made product routes and single product

--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ dotenv.config();
 const PORT = process.env.PORT;
 app.use(express.json());
 
-
 const mongoose = require("mongoose");
 
 //connect db
@@ -18,12 +17,15 @@ mongoose
 
 const databaseSeeder = require("./databaseSeeder");
 const userRoute = require("./Routes/User");
+const productRoute = require("./Routes/Product");
 //database seeder routes
 app.use("/api/seed", databaseSeeder);
 
-
 //routes for users
 app.use("/api/users", userRoute);
+
+//routes for products
+app.use("/api/products", productRoute);
 
 app.listen(PORT || 9000, () => {
   console.log(`server listening on port ${PORT}`);
