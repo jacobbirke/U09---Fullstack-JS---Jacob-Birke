d2d3ceac7dba7122123f63730e7183e7b567e9da
jacobbirke <birkejacob@gmail.com>
2024-10-02 15:23:43 +0200

    payment setup and getting order and single order

--- api/Routes/Order.js
+++ api/Routes/Order.js
@@ -21,7 +21,7 @@ orderRoute.post(
       res.status(400);
       throw new Error("Ingen order hittad");
     } else {
-      const order = new Order ({
+      const order = new Order({
         orderItems,
         shippingAddress,
         paymentMethods,
@@ -29,13 +29,66 @@ orderRoute.post(
         taxPrice,
         totalPrice,
         price,
-        user:req.user._id
+        user: req.user._id,
       });
-      const createdOrder = await order.save(); 
-      res.status(201).json(createdOrder)
+      const createdOrder = await order.save();
+      res.status(201).json(createdOrder);
     }
   })
 );
 
+//order payment route
+orderRoute.put(
+  "/:id/payment",
+  protect,
+  asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        updated_time: req.body.uptades_time,
+        email_address: req.body.email_address,
+      };
+      const updatedOrder = await order.save();
+      res.status(200).json(updatedOrder);
+    } else {
+      res.status(404);
+      throw new Error("Order ej hittad");
+    }
+  })
+);
+
+// get all orders
+orderRoute.get(
+  "/",
+  protect,
+  asyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id }).sort({ _id: -1 });
+    if (orders) {
+      res.status(200).json(orders);
+    } else {
+      res.status(404);
+      throw new Error("Ordrar ej hittad");
+    }
+  })
+);
+
+// get one order by id
+orderRoute.get(
+  "/:id",
+  protect,
+  asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate("user", "email");
+    if (order) {
+      res.status(202).json(order);
+    } else {
+      res.status(404);
+      throw new Error("Order ej hittad");
+    }
+  })
+);
 
 module.exports = orderRoute;
