31cfb86e32d383e8aced2001424bdd33c46fa9ae
jacobbirke <birkejacob@gmail.com>
2024-10-03 16:36:14 +0200

    setup of product detail ui page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import ProductDetail from './Pages/ProductDetail';
+import {
 
-createRoot(document.getElementById('root')!).render(
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App/>}></Route>
+        <Route path="/detail" element={<ProductDetail/>}></Route>
+      </Routes>
+    </Router>
+  </StrictMode>
+);
