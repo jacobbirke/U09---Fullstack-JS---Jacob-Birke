5e3d0f579be2e5c05945f87d6c22accf55409094
jacobbirke <birkejacob@gmail.com>
2024-10-26 17:04:04 +0200

    vercel frontend fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Exclude dependencies you don't want to bundle with the frontend code
+      external: []
+    },
+  }
 })
